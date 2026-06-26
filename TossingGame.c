8c8676e0660ce2165a6f59d3ff0d4b106cb2475f
Fuji-sigzl <s1330237@u-aizu.ac.jp>
2026-06-26 12:03:35 +0900

    HoT: initial revision

--- TossingGame.c
+++ TossingGame.c
@@ -10,17 +10,17 @@ int main(){
 
     printf("Tossing a coin...\n");
 
-    for(int i=0; i<=3; i++){
+    for(int i=1; i<=3; i++){
         if(rand() % 2 == 0) {
             printf("Round %d: Heads\n", i);
             heads++;
         } else {
-            printf("Round %d: tails\n", i);
+            printf("Round %d: Tails\n", i);
             tails++;
         }
     }
 
-    printf("Heads: %d, Tails: %d", heads, tails);
+    printf("Heads: %d, Tails: %d\n", heads, tails);
   
     return 0;
 }
